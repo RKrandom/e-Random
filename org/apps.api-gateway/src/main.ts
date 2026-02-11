af8bb34191f996ab37c6e417d98572c1845b804b
Raunak Kumar <rkjsr2005@gmail.com>
2026-02-11 22:50:54 +0000

    importing all micro-services

--- org/apps.api-gateway/src/main.ts
+++ org/apps.api-gateway/src/main.ts
@@ -4,12 +4,27 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+import cors from 'cors';
+import morgan from 'morgan';
+import proxy from 'http-proxy-middleware';
+import rateLimit from 'express-rate-limit';
+import cookieparser from 'cookie-parser';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
+app.use(morgan('dev'));
+app.use(express.json({  limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cookieparser());
+app.set('trust proxy', 1);
+
+ 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to apps.api-gateway!' });
 });
